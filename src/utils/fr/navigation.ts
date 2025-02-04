ebebf855fc1c46b18315709cf4ac75cec3f85d1c
Sayed Nawaz <u02jv21s0057@anjuman.edu.in>
2025-02-04 12:56:26 +0530

    fix state maintained

--- src/utils/fr/navigation.ts
+++ src/utils/fr/navigation.ts
@@ -1,28 +1,28 @@
 
 const navBarLinks = [
-  { name: "الرئيسية", url: "/" },
-  { name: "المنتجات", url: "/products" },
-  { name: "المدونة", url: "/blog" },
-  { name: "من نحن", url: "/services" },
-  { name: "اتصل بنا", url: "/contact" },
+  { name: "الرئيسية", url: "/fr" },
+  { name: "المنتجات", url: "/fr/products" },
+  { name: "المدونة", url: "/fr/blog" },
+  { name: "من نحن", url: "/fr/about" },
+  { name: "اتصل بنا", url: "/fr/contact" },
 ];
 
 const footerLinks = [
   {
     section: "روابط سريعة",
     links: [
-      { name: "المنتجات", url: "/products" },
-      { name: "من نحن", url: "/services" },
-      { name: "اتصل بنا", url: "#" },
-      { name: "المدونة", url: "/blog" },
+      { name: "المنتجات", url: "/fr/products" },
+      { name: "من نحن", url: "/fr/about" },
+      { name: "اتصل بنا", url: "fr/contact" },
+      { name: "المدونة", url: "/fr/blog" },
     ],
   },
   {
     section: "",
     links: [
-      { name: "آراء العملاء", url: "#" },
+      { name: "آراء العملاء", url: "/fr/products/#testimonials" },
       { name: "عملاؤنا", url: "#" },
-      { name: "الأسئلة الشائعة", url: "#" },
+      { name: "الأسئلة الشائعة", url: "/fr/#faqs" },
     ],
   },
 ];
